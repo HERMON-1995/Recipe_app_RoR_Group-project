721a4118b59f602d34f79e36086cb1ca93bab922
Thiri <thirihtet246@gmail.com>
2023-06-27 19:57:48 +0630

    fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,9 +6,9 @@ class RecipesController < ApplicationController
   def destroy
     @recipe = Recipe.includes(:user).find(params[:id])
 
-    if @recipe.destroy
-      flash[:notice] = 'Recipe was successfully deleted.'
-      redirect_to recipes_path
-    end
+    return unless @recipe.destroy
+
+    flash[:notice] = 'Recipe was successfully deleted.'
+    redirect_to recipes_path
   end
 end
