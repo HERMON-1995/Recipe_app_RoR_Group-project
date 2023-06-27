b3045b0a3ed37190466fa2254b0f7a8b5b994bbd
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-27 16:25:16 +0300

    Update login and sign up pages

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    render plain: 'Welcome to our app! This is the home page.'
-    @current_user = current_user
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
+
+  resources :users, only: [:index, :show] do
+    resources :foods, only: [:index, :show, :new, :create, :destroy] do
+      resources :recipefoods, only: [:create]
+    end
+    resources :recipes, only: [:create, :new, :destroy, :index]do
+      resources :recipefoods, only: [:create]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root "home#index"
 end
