dcc7e9fdd59972c873aa4cfc3642192c55550279
Thiri <thirihtet246@gmail.com>
2023-06-28 17:48:57 +0630

    Add routes for public recipes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   resources :recipes, only:  [:index, :show, :new, :create, :destroy]do
     resources :recipefoods, only: [:create]
   end
+
+  get '/public_recipes', to: 'recipes#public', as: 'public_recipes'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
