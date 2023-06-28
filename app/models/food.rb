76e343d42bfae6d0692aaf0e4aad3b2115323436
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-28 14:04:02 +0300

    Fix linter error

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,7 +3,6 @@ class Food < ApplicationRecord
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
 
-
   validates :name, presence: true
   validates :price, format: { with: /\A\$?\d+(\.\d{1,2})?\z/, message: 'must be a valid price format' }
 end
