4171d342e45456aae403cefb9a97e30726b987de
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-28 09:55:12 +0300

    Add some functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+   root "users#index"
 
-  resources :users, only: [:index, :show] do
-    resources :foods, only: [:index, :show, :new, :create, :destroy] do
-      resources :recipefoods, only: [:create]
-    end
-    resources :recipes, only: [:create, :new, :destroy, :index]do
-      resources :recipefoods, only: [:create]
-    end
+  resources :users, only: [:index, :show] do 
   end
+
+  resources :foods, only: [:index, :show, :new, :create, :destroy] 
+
+  resources :recipes, only:  [:index, :show, :new, :create, :destroy]do
+    resources :recipefoods, only: [:create]
+   end
+
+  
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # root "home#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+hermon = User.find_by(name: 'HERMON')
+
+recipe_one = Recipe.create(name: "Recipe One", description: "Recipe One Description", user_id: hermon.id)
+recipe_two = Recipe.create(name: "Recipe two", description: "Recipe two Description", user_id: hermon.id)
+recipe_three = Recipe.create(name: "Recipe three", description: "Recipe three Description", user_id: hermon.id)
+chocolate_truffle = Recipe.create(name: "Chocolate Truffle", description: "Warm Cream: Pour warm cream over chopped chocolate.\n
+    Do Nothing: Before stirring, let the warm cream and chocolate sit in the bowl for a few minutes. Don’t touch it!\n
+    Stir: Stir until the chocolate is melted.\n
+    Refrigerate: Refrigerate until the mixture is set, about 1-2 hours.\n
+    Scoop: Scoop into balls with a cookie scoop or spoon.\n
+    Roll: Roll into balls and coat in toppings like sprinkles, cocoa powder, or crushed nuts. You can also dip the truffles in melted or tempered chocolate.", user_id: hermon.id)
