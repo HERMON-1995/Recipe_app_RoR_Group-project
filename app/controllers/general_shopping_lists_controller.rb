c641804b3c954f26b6b868aecae834d38b22e258
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-29 14:18:39 +0300

    Fix shopping list code

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -18,28 +18,22 @@ class GeneralShoppingListsController < ApplicationController
     @recipes.each do |recipe|
       recipe.recipe_foods.each do |recipe_food|
         food = recipe_food.food
-        if shoppinglist_raw[food.id]
-          shoppinglist_raw[food.id][:quantity] += recipe_food.quantity
-        else
-          shoppinglist_raw[food.id] = {
-            food: food,
-            quantity: recipe_food.quantity
-          }
-        end
+        shoppinglist_raw[food.id] ||= { food:, quantity: 0 }
+        shoppinglist_raw[food.id][:quantity] += recipe_food.quantity
       end
     end
 
     shoppinglist = []
-    shoppinglist_raw.values.each do |recipe_food|
+    shoppinglist_raw.each_value do |recipe_food|
       general_food = @general_food_list.find_by(name: recipe_food[:food].name)
-      if general_food.nil? || general_food.quantity < recipe_food[:quantity]
-        quantity = recipe_food[:quantity] - (general_food&.quantity || 0)
-        shoppinglist << {
-          food: recipe_food[:food],
-          quantity: quantity,
-          price: (general_food&.price || 0) * quantity
-        }
-      end
+      next unless general_food.nil? || general_food.quantity < recipe_food[:quantity]
+
+      quantity = recipe_food[:quantity] - (general_food&.quantity || 0)
+      shoppinglist << {
+        food: recipe_food[:food],
+        quantity:,
+        price: (general_food&.price || 0) * quantity
+      }
     end
 
     shoppinglist
