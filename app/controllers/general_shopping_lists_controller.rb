544a21f3b9acdb56fa94d6a30f4b44edae239b3e
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-06-29 13:46:39 +0300

    Fix duplicating and total sum issues in general shopping list

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -2,39 +2,50 @@ class GeneralShoppingListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    recipes = current_user.recipes.includes(recipe_foods: :food)
-    shoppinglist_raw = {}
-    @shoppinglist = []
-
-    recipes.each do |recipe|
-      recipe.recipe_foods.each do |recipefood|
-        food = recipefood.food
-        if shoppinglist_raw[food.id]
-          shoppinglist_raw[food.id][:quantity] += recipefood.quantity
-        else
-          shoppinglist_raw[food.id] = {
-            food:,
-            quantity: recipefood.quantity
-          }
-        end
-      end
-
-      update_shoppinglist(shoppinglist_raw)
-    end
+    @user = current_user
+    @recipes = @user.recipes.includes(recipe_foods: :food)
+    @general_food_list = @user.foods
+    @shoppinglist = calculate_shopping_list
+    @total_items = @shoppinglist.size
+    @total_price = calculate_total_price
   end
 
   private
 
-  def update_shoppinglist(shoppinglist_raw)
-    shoppinglist_raw.values.each do |recipefood|
-      quantity = recipefood[:quantity] - recipefood[:food].quantity
-      next unless quantity.positive?
+  def calculate_shopping_list
+    shoppinglist_raw = {}
 
-      @shoppinglist << {
-        food: recipefood[:food],
-        quantity:,
-        price: recipefood[:food].price * quantity
-      }
+    @recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        food = recipe_food.food
+        if shoppinglist_raw[food.id]
+          shoppinglist_raw[food.id][:quantity] += recipe_food.quantity
+        else
+          shoppinglist_raw[food.id] = {
+            food: food,
+            quantity: recipe_food.quantity
+          }
+        end
+      end
     end
+
+    shoppinglist = []
+    shoppinglist_raw.values.each do |recipe_food|
+      general_food = @general_food_list.find_by(name: recipe_food[:food].name)
+      if general_food.nil? || general_food.quantity < recipe_food[:quantity]
+        quantity = recipe_food[:quantity] - (general_food&.quantity || 0)
+        shoppinglist << {
+          food: recipe_food[:food],
+          quantity: quantity,
+          price: (general_food&.price || 0) * quantity
+        }
+      end
+    end
+
+    shoppinglist
+  end
+
+  def calculate_total_price
+    @shoppinglist.sum { |item| item[:price] }
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   has_many :recipe_foods
-  has_many :foods, through: :recipe_foods
+  has_many :foods, through: :recipe_foods, dependent: :destroy
 
   validates :name, presence: true
   validates :description, presence: true
