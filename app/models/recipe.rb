8066bfa48763830b8daf2afe95d725fcfceddce3
Thiri <thirihtet246@gmail.com>
2023-06-29 21:32:32 +0630

    update recipe_food

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,6 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true
 end
